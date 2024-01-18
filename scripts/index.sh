6a3c83ff99fb3b146f1bea2d69727a44ca8fc876
Fernando Barquín <locofoco98@gmail.com>
2024-01-18 13:26:51 +0100

    modificado el index

--- scripts/index.sh
+++ scripts/index.sh
@@ -4,5 +4,20 @@
 # The STAR command is provided for you. You should replace the parts surrounded
 # by "<>" and uncomment it.
 
-# STAR --runThreadN 4 --runMode genomeGenerate --genomeDir <outdir> \
-# --genomeFastaFiles <genomefile> --genomeSAindexNbases 9
+
+
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <genome_file> <output_directory>"
+    exit 1
+fi
+
+genome_file=$1
+output_directory=$2
+
+mkdir -p "$output_directory"
+
+# Run STAR command for genome indexing
+STAR --runThreadN 4 --runMode genomeGenerate \
+     --genomeDir "$output_directory" \
+     --genomeFastaFiles "$genome_file" \
+     --genomeSAindexNbases 9
